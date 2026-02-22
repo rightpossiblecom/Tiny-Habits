2f66ceba3c613966f1664a36aff5e02373b85af6
Mr Right <rightpossible1@gmail.com>
2026-02-22 11:55:55 +0100

    feat: Add initial Flutter Android MainActivity and project image assets.

--- android/app/src/main/kotlin/com/example/tiny_habits/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tiny_habits
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/tinyhabits/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.tinyhabits.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
